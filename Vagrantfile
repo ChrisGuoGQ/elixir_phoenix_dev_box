2fe50cd410d53c69c9244ed3a502be6081afb4df
郭奇 <chrisguogq@gmail.com>
2017-06-17 16:05:19 +0800

    fix: add more

--- Vagrantfile
+++ Vagrantfile
@@ -23,7 +23,7 @@ Vagrant.configure("2") do |config|
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # NOTE: This will enable public access to the opened port
-  config.vm.network "forwarded_port", guest: 4000, host: 1234
+  # config.vm.network "public_network"
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine and only allow access
@@ -32,7 +32,7 @@ Vagrant.configure("2") do |config|
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
-  # config.vm.network "private_network", ip: "192.168.33.10"
+  config.vm.network "private_network", ip: "192.168.33.10"
 
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
@@ -54,7 +54,7 @@ Vagrant.configure("2") do |config|
   #   vb.gui = true
   #
   #   # Customize the amount of memory on the VM:
-    vb.memory = "2048"
+    vb.memory = "4096"
     vb.cpus = 2
   end
   #
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,32 +4,38 @@ function install {
     apt-get -y install "$@" >/dev/null 2>&1
 }
 
-echo adding swap file
-fallocate -l 4G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+# echo adding swap file
+# fallocate -l 4G /swapfile
+# chmod 600 /swapfile
+# mkswap /swapfile
+# swapon /swapfile
+# echo '/swapfile none swap defaults 0 0' >> /etc/fstab
 
 echo add erlang-solutions
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 apt-get -qq update
+# install Zsh zsh
+# sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+# sudo usermod -s /bin/zsh username
 install Erlang esl-erlang
 install Erlang-ssl erlang-ssl
+install Erlang-dev erlang-dev
+install Erlang-parsetools erlang-parsetools
 install Elixir elixir
 install Inotify inotify-tools
 install Git git
 install Redis redis-server
+install Build-essential build-essential
 
 install PostgreSQL postgresql postgresql-contrib libpq-dev
 # sudo -u postgres createuser --pwprompt deploy
 
-echo mix
-yes | mix local.hex
-yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+# echo mix
+# yes | mix local.hex
+# yes | mix local.rebar
+# yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
 
-echo nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+# wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 # nvm install node
 # npm install -g yarn
 
